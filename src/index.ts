ff63f026d64aaee565dd2430895fe6fd30164bb8
gumingWu <15626185054@163.com>
2023-04-06 21:03:34 +0800

    get and save github token

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-export const one = 1
-export const two = 2
+import { simpleGit } from 'simple-git'
+import { intro, note, outro, text } from '@clack/prompts'
+
+const GIT_TOKEN_KEY = 'pushGithub.githubToken'
+const git = simpleGit()
+
+async function getGithubToken() {
+  const getTokenResult = await git.getConfig(GIT_TOKEN_KEY, 'global')
+  let token = getTokenResult.value
+  if (!token)
+    token = await saveGithubToken()
+
+  return token
+}
+
+async function saveGithubToken(): Promise<string> {
+  const token = await text({
+    message: 'Paste your github token',
+  })
+  await git.addConfig(GIT_TOKEN_KEY, token as string, false, 'global')
+  return token as string
+}
+
+async function main() {
+  intro('Push Github')
+  const token = await getGithubToken()
+
+  if (token)
+    note(token)
+
+  outro('Finish')
+}
+
+main()

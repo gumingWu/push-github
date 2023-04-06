8b0990180eef35d3919cc105d39d6ba945edb3be
gumingWu <15626185054@163.com>
2023-04-06 23:26:40 +0800

    create and push project to github

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { simpleGit } from 'simple-git'
-import { intro, note, outro, text } from '@clack/prompts'
+import { cancel, intro, note, text, spinner } from '@clack/prompts'
+import { readJSON } from 'fs-extra'
+import { Octokit } from '@octokit/core'
+import type { OctokitResponse } from '@octokit/types'
 
 const GIT_TOKEN_KEY = 'pushGithub.githubToken'
 const git = simpleGit()
+const spinnerInstance = spinner()
 
 async function getGithubToken() {
   const getTokenResult = await git.getConfig(GIT_TOKEN_KEY, 'global')
@@ -21,14 +25,68 @@ async function saveGithubToken(): Promise<string> {
   return token as string
 }
 
+async function resolveProjectName() {
+  const json = await readJSON('./package.json').catch((err) => {
+    if (err.message.includes('no such file or directory')) {
+      cancel('no package.json in this folder, please check the path')
+      return null
+    }
+    throw err
+  })
+
+  if (json) {
+    const packageJsonProjectName = (json.name && json.name !== '') ? json.name : 'push-github-project'
+    const projectName = await text({
+      message: 'Input your github repository name, if you use initial value, just press enter',
+      initialValue: packageJsonProjectName,
+    })
+    return projectName
+  }
+  return 'push-github-project'
+}
+
+async function createGithubRepository(name: string, auth: string) {
+  spinnerInstance.start('Starting create github repository')
+
+  const octokit = new Octokit({
+    auth,
+  })
+  const res = await octokit.request('POST /user/repos', {
+    name,
+  }).catch((err) => {
+    spinnerInstance.stop(`Create repository fail: ${err.message}`)
+  })
+  const htmlUrl = (res as OctokitResponse<any>).data.html_url
+
+  spinnerInstance.stop(`Create github repository successfully, url: ${htmlUrl}`)
+
+  return htmlUrl
+}
+
+async function pushProject(url: string) {
+  await git
+    .init()
+    .add('./*')
+    .commit('first commit by push-github')
+    .branch(['main'])
+    .addRemote('origin', url)
+    .push(['-u', 'origin', 'main'])
+    .catch((err) => {
+      note(`Git push fail: ${err.message}`, 'error')
+    })
+}
+
 async function main() {
   intro('Push Github')
   const token = await getGithubToken()
 
-  if (token)
-    note(token)
+  if (token) {
+    const name = await resolveProjectName()
+    const url = await createGithubRepository(name as string, token)
+    pushProject(url)
+  }
 
-  outro('Finish')
+  // outro('Finish')
 }
 
 main()
